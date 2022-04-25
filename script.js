38dbc9c969708dbbf8b7f6e667d417cef42f96a2
wihtlake <wihtlake@mail.ru>
2022-04-25 06:21:00 +0500

    disign and animations

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ async function getNews(categ){
     console.log(`getNews даные об аргументах  ${categ}  и номер ${indexmass}`);
     // создаем массив для категорий
     
-
     let url = `${addres}${country}${list_cat}${categ}${api}`;
     const response = await fetch(url);
     const data = await response.json();
@@ -34,112 +33,88 @@ async function getNews(categ){
   }
 
 function startNews(newsBox){
-  let i = 0;
-  for (i = 1; i < newsBox.length; i++) {
-    const author = newsBox[i].author;
-    const content = newsBox[i].content;
-    const description = newsBox[i].description;
-    const title = newsBox[i].title;
-    const urlpage = newsBox[i].url;
-    const urlToImage = newsBox[i].urlToImage;
+    let i = 0;
+    for (i = 1; i < newsBox.length; i++) {
+      const author = newsBox[i].author;
+      const content = newsBox[i].content;
+      const description = newsBox[i].description;
+      const title = newsBox[i].title;
+      const urlpage = newsBox[i].url;
+      const urlToImage = newsBox[i].urlToImage;
+      const publishedAt = newsBox[i].publishedAt;
+      console.log(urlToImage)
+      console.log('создаем элементы на страницу')
 
-    console.log('создаем элементы на страницу')
+      const newTask = document.createElement('div');
+      newTask.classList.add('news_box');
+      box.appendChild(newTask);
+  
+      const imageBox = document.createElement('img');
+      imageBox.classList.add('image');
+      imageBox.setAttribute('src', urlToImage);
+      imageBox.setAttribute('alt', '');
+      newTask.append(imageBox);
+      
+      
 
-    const newTask = document.createElement('div');
-    newTask.classList.add('news_box');
-    box.appendChild(newTask);
+      const titleBox = document.createElement('a');
+      titleBox.classList.add('title');
+      titleBox.setAttribute('href', urlpage);
+      titleBox.setAttribute('target', '_blank');
 
-    const imageBox = document.createElement('img');
-    imageBox.classList.add('image');
-    imageBox.setAttribute('src', urlToImage);
-    newTask.append(imageBox);
+      titleBox.innerText = title;
+      newTask.append(titleBox);
 
-    const titleBox = document.createElement('h2');
-    titleBox.classList.add('title');
-    titleBox.innerText = title;
-    newTask.append(titleBox);
-    
-    const urlBox = document.createElement('a');
-    urlBox.innerText = description;
-    urlBox.setAttribute('href', urlpage);
-    newTask.append(urlBox);
-
-    const contentBox = document.createElement('p');
-    contentBox.classList.add('content');
-    contentBox.innerText = content;
-    newTask.append(contentBox);
-
-    const descriptionBox = document.createElement('p');
-    descriptionBox.classList.add('description');
-    descriptionBox.innerText = description;
-  }
+      const urlBox = document.createElement('a');
+      urlBox.classList.add('urlBox');
+      urlBox.innerText = description;
+      newTask.append(urlBox);
+    }
 } 
 function detele22(){
   let i = 0;
-  console.log(document.querySelectorAll('.news_box').length);
-
   for (i = 0; i < document.querySelectorAll('.news_box').length; i++) {
-    console.log(i);
     document.querySelector('.news_box').remove();
     detele22();
-    // ещё выражения
 }
 }
 
-// example1.js
 function go(){
-  setTimeout(
-    () => {
-        console.log('удаляем старые компоненты... ');
-        detele22();
-        check();
-      },
-      1
-    );
+  detele22();
+  check();
 }
 
 function check(){
-  setTimeout(
-() => {
-    
     if(document.querySelector('.news_box')){
       console.log('newsBox найден!');
       go();
     }else{
       console.log('newsBox не найден!');
-
       console.log('добавляем новые компоненты... ');
       connect();
     }
-    
-  },
-  1
-);
 }
 check();
 }
-
 
 function delete1(categ, indexmass){
   console.log(`delete1 даные об аргументах  ${categ} + ${indexmass}`)
   console.log(`число для сравнения ${indexmass}`);
 }
 
- const mass = [ '' ,'business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
-
 function elem1(categ, indexmass){
   delete1(categ, indexmass)
-  console.log(`elem1 даные об аргументах  ${categ} и ${indexmass}`)
-  console.log(`запускаем getNews`);
   getNews(categ);
 }
 
+const mass = [ '' ,'business', 'entertainment', 'general', 'science', 'sports', 'technology'];
 
 btn.onclick = function(categ, newTask){
   categ = mass[catBox.value]
   indexmass = catBox.value;
-  console.log(` нажали на кнопку и присвоили значение categ: ${categ}`);
-  console.log(` нажали на кнопку и нашли значение массива indexmass: ${indexmass}`);
-  console.log(`передели два значения в elem1 ${categ} ${indexmass}`)
-  elem1(categ, indexmass);
+  if(indexmass == 0){
+    alert('Выберите категорию');
+  }else{
+    elem1(categ, indexmass);
+  }
 }
